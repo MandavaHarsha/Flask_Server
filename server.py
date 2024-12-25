35366e0f186c2c3ae381babc580e60a57cd35141
Harsha <108451524+MandavaHarsha@users.noreply.github.com>
2024-12-25 23:23:52 +0530

    Updated portnumbers

--- server.py
+++ server.py
@@ -178,5 +178,6 @@ def get_liked_songs():
     return jsonify(liked_songs)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
